f79c03f807c1fbbda5351c7181e43c1f96ac2e12
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-08-04 15:20:32 +0800

    refine

--- app/src/main/java/org/lsposed/lspromise/MainActivity.java
+++ app/src/main/java/org/lsposed/lspromise/MainActivity.java
@@ -4,13 +4,18 @@ import static org.lsposed.lspromise.Shellcode.TAG;
 
 import android.app.Activity;
 import android.content.BroadcastReceiver;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Parcel;
+import android.os.RemoteException;
 import android.telecom.PhoneAccount;
 import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
@@ -50,6 +55,49 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    private void runAll() {
+        if (controller != null) {
+            new Thread(() -> {
+                var p = Parcel.obtain();
+                var r = Parcel.obtain();
+                var b = new Binder() {
+                    @Override
+                    protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+                        try {
+                            var s = data.readString();
+                            Log.d(TAG, "onTransact " + s);
+                            runOnUiThread(() -> {
+                                tv.append(s);
+                            });
+                        } catch (Throwable t) {
+                            Log.e(TAG, "recv failed", t);
+                        }
+                        return true;
+                    }
+                };
+                p.writeStrongBinder(b);
+                try {
+                    if (controller.transact(5, p, r, 0)) {
+                        var res = r.readInt();
+                        runOnUiThread(() -> {
+                            tv.append("\nrunall done res=" + res + "\n");
+                        });
+                    } else {
+                        throw new IllegalStateException("return false");
+                    }
+                } catch (Throwable t) {
+                    Log.e(TAG, "runall failed", t);
+                    runOnUiThread(() -> {
+                        tv.append("runall failed: " + t.getMessage() + "\n");
+                    });
+                } finally {
+                    p.recycle();
+                    r.recycle();
+                }
+            }).start();
+        }
+    }
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
@@ -75,6 +123,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
         patchCxx.setOnClickListener(v -> doAction(3, "patchCxx"));
         var forkProcess = (Button) findViewById(R.id.forkProcess);
         forkProcess.setOnClickListener(v -> doAction(4, "forkProcess"));
+        var patchAll = (Button) findViewById(R.id.patchAll);
+        patchAll.setOnClickListener(v -> {
+            runAll();
+        });
+        var copyAll = (Button) findViewById(R.id.copyAll);
+        copyAll.setOnClickListener(v -> {
+            var cm = getSystemService(ClipboardManager.class);
+            cm.setPrimaryClip(ClipData.newPlainText("", tv.getText().toString()));
+        });
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -86,6 +143,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     patchLibc.setVisibility(View.VISIBLE);
                     patchCxx.setVisibility(View.VISIBLE);
                     forkProcess.setVisibility(View.VISIBLE);
+                    patchAll.setVisibility(View.VISIBLE);
                 } catch (Throwable t) {
                     Log.e(TAG, "resolve controller", t);
                 }
